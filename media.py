a3632149a186a8057591614ce00b0c0eaf221314
ng10 <nganani@gmail.com>
2017-06-05 14:48:51 +0300

    More clean comments for media and entertainment_center python files

--- media.py
+++ media.py
@@ -3,11 +3,9 @@ import webbrowser
 
 class Movie():
     # Documentation for the class in triple quotes:
-    """ Class to store movie related data """
+    """ Class to store movie related data. play trailer and potentially much more"""
 
-    # constant should be in upper case
-    VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
-
+    # Constructor for class Movie
     def __init__(
             self, movie_title, movie_storyline,
             poster_image, trailer_youtube):
@@ -16,5 +14,6 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # Play movie trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
