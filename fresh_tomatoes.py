507411d81e84f83e28bd6e9811b44a9f52d391de
ng10 <nganani@gmail.com>
2017-06-06 23:00:32 +0300

    Changed fresh_tomatoes.py to support PEP8

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -12,45 +12,42 @@ main_page_head = '''
     <title>Fresh Tomatoes!</title>
 
     <!-- Bootstrap 3 -->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/
-    3.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/
-    3.1.0/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href={boot_c}>
+    <link rel="stylesheet" href={boot_t}>
     <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/
-    bootstrap.min.js"></script>
+    <script src={boot_j}></script>
     <style type="text/css" media="screen">
-        body {
+        body {{
             padding-top: 80px;
-        }
-        #trailer .modal-dialog {
+        }}
+        #trailer .modal-dialog {{
             margin-top: 200px;
             width: 640px;
             height: 480px;
-        }
-        .hanging-close {
+        }}
+        .hanging-close {{
             position: absolute;
             top: -12px;
             right: -12px;
             z-index: 9001;
-        }
-        #trailer-video {
+        }}
+        #trailer-video {{
             width: 100%;
             height: 100%;
-        }
-        .movie-tile {
+        }}
+        .movie-tile {{
             margin-bottom: 20px;
             padding-top: 20px;
-        }
-        .movie-tile:hover {
+        }}
+        .movie-tile:hover {{
             background-color: #EEE;
             cursor: pointer;
-        }
-        .scale-media {
+        }}
+        .scale-media {{
             padding-bottom: 56.25%;
             position: relative;
-        }
-        .scale-media iframe {
+        }}
+        .scale-media iframe {{
             border: none;
             height: 100%;
             position: absolute;
@@ -58,35 +55,35 @@ main_page_head = '''
             left: 0;
             top: 0;
             background-color: white;
-        }
+        }}
     </style>
     <script type="text/javascript"> //Nir> charset not required here (W3C)
         // Pause the video when the modal is closed
         $(document).on('click', '.hanging-close, .modal-backdrop, .modal',
-        function (event) {
+        function (event) {{
             // Remove the src so the player itself gets removed, as this is
             //the only reliable way to ensure the video stops playing in IE
             $("#trailer-video-container").empty();
-        });
+        }});
         // Start playing the video whenever the trailer modal is opened
-        $(document).on('click', '.movie-tile', function (event) {
+        $(document).on('click', '.movie-tile', function (event) {{
             var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
             var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId
             + '?autoplay=1&html5=1';
             $("#trailer-video-container").empty().append($("<iframe></iframe>",
-            {
+            {{
               'id': 'trailer-video',
               'type': 'text-html',
               'src': sourceUrl,
               'frameborder': 0
-            }));
-        });
+            }}));
+        }});
         // Animate in the movies when the page loads
-        $(document).ready(function () {
-          $('.movie-tile').hide().first().show("fast", function showNext() {
+        $(document).ready(function () {{
+          $('.movie-tile').hide().first().show("fast", function showNext() {{
             $(this).next("div").show("fast", showNext);
-          });
-        });
+          }});
+        }});
     </script>
 </head>
 '''
@@ -102,10 +99,7 @@ main_page_content = '''
         <div class="modal-content">
           <a href="#" class="hanging-close" data-dismiss="modal"
           aria-hidden="true">
-            <img src="https://lh5.ggpht.com/
-            v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkof
-            p_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"
-            alt = "x icon - close modal window"/>
+            <img src={x_icon} alt = "x icon - close modal window"/>
           </a>
           <div class="scale-media" id="trailer-video-container">
           </div>
@@ -143,6 +137,7 @@ data-toggle="modal" data-target="#trailer">
 
 
 def create_movie_tiles_content(movies):
+
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
@@ -158,21 +153,40 @@ def create_movie_tiles_content(movies):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
         )
     return content
 
 
 def open_movies_page(movies):
+    # Some long strings constants
+    x_icon_pic = ("https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_"
+                  "MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_="
+                  "s0#w=24&h=24")
+
+    boot_css = ("https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/"
+                "bootstrap.min.css")
+    boot_theme = ("https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/"
+                  "bootstrap-theme.min.css")
+    boot_js = ("https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/"
+               "bootstrap.min.js")
+
     # Create or overwrite the output file
     output_file = open('fresh_tomatoes.html', 'w')
 
+    # Replace long strings on main page head
+    head_section = main_page_head.format(
+        boot_c=boot_css,
+        boot_t=boot_theme,
+        boot_j=boot_js)
+
     # Replace the movie tiles placeholder generated content
     rendered_content = main_page_content.format(
-        movie_tiles=create_movie_tiles_content(movies))
+        movie_tiles=create_movie_tiles_content(movies),
+        x_icon=x_icon_pic)
 
     # Output the file
-    output_file.write(main_page_head + rendered_content)
+    output_file.write(head_section + rendered_content)
     output_file.close()
 
     # open the output file in the browser (in a new tab, if possible)
